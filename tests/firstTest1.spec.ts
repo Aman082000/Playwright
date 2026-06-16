7f927f61a08332442635166ac3e7608b12e122e7
Aman082000 <aman.bhardwaj0820@gmail.com>
2026-06-16 22:52:58 +0530

    UserFacingLocators

--- tests/firstTest1.spec.ts
+++ tests/firstTest1.spec.ts
@@ -24,25 +24,43 @@ test.describe('Test Suite 2',()=>{
     test.beforeEach(async({page})=>{
         await page.goto('http://localhost:4200/')
         await page.getByText('Forms').click()
+        await page.getByText('Form Layouts').click()
     })
 
     test('Locator syntax rules',async({page})=>{
         
-        await page.locator('input').click()
+        await page.locator('input')
 
         await page.locator('#inputEmail1').click()
 
-        await page.locator('.shape-rectangle').click()
+        await page.locator('.shape-rectangle')
 
-        await page.locator('[placeholder="Email"]').click()
+        await page.locator('[placeholder="Email"]')
 
-        await page.locator('[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]').click()
+        await page.locator('[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]')
 
-        await page.locator('input[placeholder="Email"].shape-rectangle').click()
+        await page.locator('input[placeholder="Email"].shape-rectangle')
 
+        await page.locator(':text-is("Using the Grid")')
 
+        await page.locator(':text("Using")')
     
     })
 
+    test('User Facing Locators', async({page})=>{
+        await page.getByRole('textbox', {name: "Email"}).first().click()
+
+        await page.getByRole('button', {name: "Sign In"}).first().click()
+
+        await page.getByLabel("Email").first().click()
+
+        await page.getByPlaceholder('Jane Doe').first().click()
+
+        await page.getByText('Using the Grid').click()
+
+        await page.getByTestId('SignIn').click()
+
+        await page.getByTitle('IoT Dashboard').click()
+    })
 
 })
