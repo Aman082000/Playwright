1b446cdc7103de93c92bf265ce57bfe164f5ca6b
Aman082000 <aman.bhardwaj0820@gmail.com>
2026-06-19 22:38:21 +0530

    AutoWait

--- tests/autoWaiting.spec.ts
+++ tests/autoWaiting.spec.ts
@@ -1,6 +1,6 @@
 import {test, expect} from '@playwright/test'
 
-test.describe('Second Suite', async()=>{
+test.describe('First Suite', async()=>{
 
       test.beforeEach(async({page})=>{
         await page.goto('http://uitestingplayground.com/ajax')
@@ -13,10 +13,27 @@ test.describe('Second Suite', async()=>{
         // await successMsg.click()
         // const successTxt = await success.textContent()
        
-        await success.waitFor({state: "attached"})
+        // await success.waitFor({state: "attached"})
+
+        // const successTxt = await success.allTextContents()
+        // expect(successTxt).toEqual(["Data loaded with AJAX get request."])
+
+        //Locator Assertion
+        await expect (success).toHaveText('Data loaded with AJAX get request.',{timeout:200000})
+
+    })
+
+    test('alternative waits', async({page})=>{
+        const success = page.locator('.bg-success')
+
+        // await page.waitForSelector('.bg-success')
+        // await page.waitForResponse('http://uitestingplayground.com/ajaxdata')
+
+        await page.waitForLoadState('networkidle')
 
         const successTxt = await success.allTextContents()
         expect(successTxt).toEqual(["Data loaded with AJAX get request."])
 
     })
+
 })
