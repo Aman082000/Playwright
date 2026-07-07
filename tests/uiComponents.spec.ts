9d5d373bf906fd481490b4cc48a161e74ce3d684
Aman082000 <aman.bhardwaj0820@gmail.com>
2026-07-07 21:21:45 +0530

    WebTables2

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -150,3 +150,63 @@ test('Dialog boxes', async({page})=>{
     expect(page.locator('table tr').first()).not.toHaveText('mdo@gmail.com')
 
 })
+
+test('Web tables Part 1', async({page})=>{
+    await page.getByText('Tables & Data').click()
+    await page.getByText('Smart Table').click()
+
+    //Get the row 
+    const thirdRow = page.getByRole('row', {name: 'twitter@outlook.com'})
+    
+    //Click on pencil icon
+    await thirdRow.locator('.nb-edit').click()
+
+   
+    await page.locator('input-editor').getByPlaceholder('Age').clear()
+    await page.locator('input-editor').getByPlaceholder('Age').fill('36')
+
+    await page.locator('.nb-checkmark').click()
+
+    //await page.pause()
+
+    //Next use case
+    await page.locator('.ng2-smart-pagination-nav').getByText('2').click()
+
+
+    const targetRow = page.getByRole('row', {name: "11"}).filter({has: page.locator('td').nth(1).getByText('11')})
+    await targetRow.locator('.nb-edit').click()
+
+   await page.locator('input-editor').getByPlaceholder('E-mail').clear()
+   await page.locator('input-editor').getByPlaceholder('E-mail').fill('saksam0808@gmail.com')
+
+   await page.locator('.nb-checkmark').click()
+
+   await expect(targetRow.locator('td').nth(5)).toHaveText('saksam0808@gmail.com')
+
+    //await page.pause() 
+})
+
+
+test('Web tables Part 2', async({page})=>{
+    await page.getByText('Tables & Data').click()
+    await page.getByText('Smart Table').click()
+
+    const ages = ['20', '30', '40', '200']
+    for(const age of ages){
+        await page.locator('input-filter').getByPlaceholder('Age').clear()
+        await page.locator('input-filter').getByPlaceholder('Age').fill(age)
+        await page.waitForTimeout(500)
+
+        const tableRows = page.locator('tbody tr')
+        for(const row of await tableRows.all()){
+            const textValue = await page.locator('td').last().textContent()
+            if(age=='200'){
+                   // expect(textValue).toEqual(" No data found ")
+                expect(await page.getByRole('table').textContent()).toContain('No data found')
+            }
+            else{
+                expect(textValue).toEqual(age)
+            }
+        }
+    }
+})
