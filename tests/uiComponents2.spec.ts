e05fd3c8de080230bcb33bd52681cf28f91255c3
Aman082000 <aman.bhardwaj0820@gmail.com>
2026-07-14 10:13:20 +0530

    Drag&Drop

--- tests/uiComponents2.spec.ts
+++ tests/uiComponents2.spec.ts
@@ -7,17 +7,30 @@ test.beforeEach(async({page})=>{
 test('sliders', async({page})=>{
     
     //1. Update attribute
-    const tempGauge = await page.locator('[tabtitle="Temperature"] ngx-temperature-dragger circle')
+    // const tempGauge = await page.locator('[tabtitle="Temperature"] ngx-temperature-dragger circle')
     
-    tempGauge.evaluate( node =>{
+    // tempGauge.evaluate( node =>{
         
-        node.setAttribute('cx', '232.60')
-        node.setAttribute('cy', '232.60')
+    //     node.setAttribute('cx', '232.60')
+    //     node.setAttribute('cy', '232.60')
 
-    })
+    // })
 
-    tempGauge.click()
+    // tempGauge.click()
 
     //2. Actual mouse movement
+    const tempBox = await page.locator('[tabtitle="Temperature"] ngx-temperature-dragger')
+    await tempBox.scrollIntoViewIfNeeded()
+
+    const box = await tempBox.boundingBox()
+    const x = box.x + box.width/2
+    const y = box.y + box.height/2
+
+    await page.mouse.move(x,y)
+    await page.mouse.down()
+    await page.mouse.move(x+200, y)
+    await page.mouse.move(x+200, y+100)
+    await page.mouse.up()
     
+    await expect(tempBox).toContainText('28')    
 })
